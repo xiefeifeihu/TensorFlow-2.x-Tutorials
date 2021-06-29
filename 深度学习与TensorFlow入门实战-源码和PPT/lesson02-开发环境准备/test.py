bbbdbcbf5587b966f817dc1f424f401bd1264dae
xiewei <xiefeifeihu@qq.com>
2021-06-29 13:40:25 +0800

    注释

--- 深度学习与TensorFlow入门实战-源码和PPT/lesson02-开发环境准备/test.py
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson02-开发环境准备/test.py
@@ -1,12 +1,12 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 
-
-
 a = tf.constant(1.)
 b = tf.constant(2.)
-print(a+b)
+print(a + b)
 
 print('GPU:', tf.test.is_gpu_available())
+print('GPU---', tf.config.list_physical_devices('CPU'))
--- 深度学习与TensorFlow入门实战-源码和PPT/lesson04-回归问题实战/linear_regression.py
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson04-回归问题实战/linear_regression.py
@@ -1,19 +1,17 @@
 import numpy as np
 
 
-
-
+# 计算损失
 # y = wx + b
 def compute_error_for_line_given_points(b, w, points):
     totalError = 0
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
-        # computer mean-squared-error
+        # computer mean-squared-error：∑（y-y‘）²
         totalError += (y - (w * x + b)) ** 2
-    # average loss for each point
+    # average loss for each point：均值
     return totalError / float(len(points))
-
 
 
 def step_gradient(b_current, w_current, points, learningRate):
@@ -23,14 +21,16 @@ def step_gradient(b_current, w_current, points, learningRate):
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
-        # grad_b = 2(wx+b-y)
-        b_gradient += (2/N) * ((w_current * x + b_current) - y)
-        # grad_w = 2(wx+b-y)*x
-        w_gradient += (2/N) * x * ((w_current * x + b_current) - y)
-    # update w'
+        # grad_b = ∂L/∂b = (2/N)∑(wxᵢ+b-yᵢ)
+        b_gradient += (2 / N) * ((w_current * x + b_current) - y)
+        # grad_w = ∂L/∂w = (2/N)∑(wxᵢ+b-yᵢ)*xᵢ
+        w_gradient += (2 / N) * x * ((w_current * x + b_current) - y)
+    # update b' = b - lr * （∂L/∂b）
     new_b = b_current - (learningRate * b_gradient)
+    # update w' = w - lr * （∂L/∂w）
     new_w = w_current - (learningRate * w_gradient)
     return [new_b, new_w]
+
 
 def gradient_descent_runner(points, starting_b, starting_w, learning_rate, num_iterations):
     b = starting_b
@@ -42,11 +42,10 @@ def gradient_descent_runner(points, starting_b, starting_w, learning_rate, num_i
 
 
 def run():
-	
     points = np.genfromtxt("data.csv", delimiter=",")
     learning_rate = 0.0001
-    initial_b = 0 # initial y-intercept guess
-    initial_w = 0 # initial slope guess
+    initial_b = 0  # initial y-intercept guess
+    initial_w = 0  # initial slope guess
     num_iterations = 1000
     print("Starting gradient descent at b = {0}, w = {1}, error = {2}"
           .format(initial_b, initial_w,
@@ -58,6 +57,7 @@ def run():
           format(num_iterations, b, w,
                  compute_error_for_line_given_points(b, w, points))
           )
+
 
 if __name__ == '__main__':
     run()
